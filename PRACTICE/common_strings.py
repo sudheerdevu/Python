56e0dabe9473d87d3603f1b65a25f5b5b513eb21
vk <sudheerdevu.vs3@gmail.com>
2024-05-07 21:58:19 +0530

    .

--- PRACTICE/common_strings.py
+++ PRACTICE/common_strings.py
@@ -1,10 +1,10 @@
 s1 = input("Enter the first string: ")
 s2 = input("Enter the second string: ")
 
-common_characters = []
+common_characters = ""
 
 for char in s1:
     if char in s2 and char not in common_characters:
-        common_characters.append(char)
+        common_characters+=char
 
-print("Characters ", ''.join(common_characters))
+print(common_characters)
