4d05135d1f03baf22335637d02c2f8c9f1182ef9
vk <sudheerdevu.vs3@gmail.com>
2024-05-14 11:18:45 +0530

    .

--- PRACTICE/assume_total.py
+++ PRACTICE/assume_total.py
@@ -1,3 +1,11 @@
 s=input()
 total = sum(int(n) for n in s.split(","))
 print(total)
+'''for i in "sudheer":
+    print(i)
+#basic loops
+print(1000);print("1000")'''
+'''marks=int(input())
+total=int(input())
+percentage= marks//total
+print(marks)'''
